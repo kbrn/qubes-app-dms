1bd20678eb9e829173fb178e8b637fa9042582f4
Andrew <kyboren@riseup.net>
2016-07-24 00:01:22 +0200

    Added lockscreen timeout monitor.

---
+++ src/qubes-app-dms-timeout.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+curstate="UNBLANK"
+oldtime=$(date '+%s')
+
+while true
+do
+    xscreensaver-command -watch
+    sleep 1
+done | /usr/libexec/qubes/heartbeat | while read line
+do
+    if [ "$(echo $line | egrep '^(LOCK|UNBLANK)')" != "" ]
+    then
+        curstate=$(echo "$line" | awk '{print $1}')
+        if [ "$curstate" != "UNBLANK" ]
+        then
+            oldtime=$(date '+%s')
+        fi
+    fi
+
+    if [ "$curstate" == "LOCK" ]
+    then
+            curtime=$(date '+%s')
+            timediff=$(($curtime - $oldtime))
+            if [ $timediff -gt 7200 ]
+            then
+                echo 1 | sudo /usr/libexec/qubes/dom0-dms-activate
+            fi
+    fi
+done
